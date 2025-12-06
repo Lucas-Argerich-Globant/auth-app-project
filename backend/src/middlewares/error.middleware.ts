1f4441da798cd6207aa6896c940c3ff8465eb2e5
Lucas Argerich <lucas.argerich@globant.com>
2025-12-06 18:11:30 -0300

    Fix add NextFunction parameter to errorMiddleware for detection as an error middleware

--- backend/src/middlewares/error.middleware.ts
+++ backend/src/middlewares/error.middleware.ts
@@ -1,6 +1,14 @@
-import type { Request, Response } from 'express'
+import type { NextFunction, Request, Response } from 'express'
+import { ZodError } from 'zod'
 
-export default function errorMiddleware(err: Error, _req: Request, res: Response) {
+export default function errorMiddleware(err: Error, _req: Request, res: Response, _next: NextFunction) {
+  if (err instanceof ZodError) {
+    return res.status(400).json({
+      status: 'error',
+      message: 'Invalid request data',
+    })
+  }
+  
   console.error('Error:', err)
   return res.status(500).json({
     status: 'error',
