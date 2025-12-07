6c124a109a5b30c3abe3cac08121b5eb9f41fd82
Lucas Argerich <lucas.argerich@globant.com>
2025-12-07 15:46:37 -0300

    Add authentication components for login and registration (empty)

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { RegisterComponent } from './auth/register/register'
+import { LoginComponent } from './auth/login/login'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    children: [
+      {
+        path: 'register',
+        component: RegisterComponent,
+        title: 'Register'
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+        title: 'Login'
+      }
+    ]
+  }
+]
