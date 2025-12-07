9e4d3cf9cb50fab8e48f8825ac6798036a2b9558
Lucas Argerich <lucas.argerich@globant.com>
2025-12-07 14:22:06 -0300

    Create install-all and dev script in package.json using concurrently

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import config from './config/config.ts'
 import app from './app.ts'
+import chalk from 'chalk'
 
 app.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`)
+  console.log(chalk.bold('Local') + ': ' + chalk.green(`http://localhost:${chalk.bold(config.port)}/`))
 })
