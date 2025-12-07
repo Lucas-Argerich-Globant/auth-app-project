64cf406706d07cdca0c91e4d452e5ac2bcb1d1ea
Lucas Argerich <lucas.argerich@globant.com>
2025-12-07 16:03:05 -0300

    Add AuthComponent and update routing for authentication

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router'
 import { RegisterComponent } from './auth/register/register'
 import { LoginComponent } from './auth/login/login'
+import { AuthComponent } from './auth/auth'
 
 export const routes: Routes = [
   {
     path: 'auth',
+    component: AuthComponent,
     children: [
       {
         path: 'register',
@@ -15,6 +17,10 @@ export const routes: Routes = [
         path: 'login',
         component: LoginComponent,
         title: 'Login'
+      },
+      {
+        path: '**',
+        redirectTo: 'login'
       }
     ]
   }
