d121a304e3d1301ffde13e95d4e52d7dec7a19e2
Lucas Argerich <lucas.argerich@globant.com>
2025-12-08 12:58:09 -0300

    Custom input component

---
+++ frontend/src/app/components/ui/input/input.ts
@@ -0,0 +1,46 @@
+import { Component, input } from '@angular/core'
+import { FormControl, ReactiveFormsModule } from '@angular/forms'
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.html',
+  imports: [ReactiveFormsModule]
+})
+export class InputComponent {
+  name = input('Name')
+  type = input<HTMLInputElement['type']>('text')
+  label = input('Label')
+  placeholder = input('Placeholder')
+  control = input<FormControl>(new FormControl(''))
+
+  hasError() {
+    return this.control().invalid && this.control().touched
+  }
+
+  errorMessage() {
+    const errors = this.control().errors
+    if (!errors) {
+      return null
+    }
+    switch (Object.keys(errors)[0]) {
+      case 'required':
+        return 'This field is required'
+      case 'email':
+        return 'Please enter a valid email address'
+      case 'minlength':
+        return `Minimum length is ${errors['minlength'].requiredLength}`
+      case 'maxlength':
+        return `Maximum length is ${errors['maxlength'].requiredLength}`
+      case 'min':
+        return `Minimum value is ${errors['min'].min}`
+      case 'max':
+        return `Maximum value is ${errors['max'].max}`
+      case 'pattern':
+        return 'Invalid format'
+      case 'mismatch':
+        return 'Values do not match'
+      default:
+        return 'Invalid field'
+    }
+  }
+}
