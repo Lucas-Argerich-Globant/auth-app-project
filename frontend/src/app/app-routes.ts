ef10238d22cbbd658deaf70c81fd6c72a2d5aba7
Lucas Argerich <lucas.argerich@globant.com>
2025-12-09 21:46:56 -0300

    Rename auth.routes to auth-routes to match best practices. Abstracted auth-routes

---
+++ frontend/src/app/app-routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router'
+import { AuthComponent } from './auth/auth'
+import { UnAuthenticatedRequired } from './auth/auth-required'
+
+export const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthComponent,
+    loadChildren: () => import('./auth/auth-routes')
+  }
+]
---
+++ frontend/src/app/auth/auth-routes.ts
@@ -0,0 +1,22 @@
+import { Routes } from '@angular/router'
+import { RegisterComponent } from './register/register'
+import { LoginComponent } from './login/login'
+
+export const routes: Routes = [
+  {
+    path: 'register',
+    component: RegisterComponent,
+    title: 'Register'
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Login'
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+]
+
+export default routes
