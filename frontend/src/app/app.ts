a8ea11425735497e8dcb77353b59deba3f0475c9
Lucas Argerich <lucas.argerich@globant.com>
2025-12-10 14:58:10 -0300

    Add app navbar

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,18 @@
-import { Component, signal } from '@angular/core'
-import { RouterOutlet } from '@angular/router'
+import { Component, inject, signal } from '@angular/core'
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router'
+import { AuthStore } from './auth/auth-store'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html'
 })
 export class App {
-  protected readonly title = signal('frontend')
+  protected authStore = inject(AuthStore)
+  protected router = inject(Router)
+
+  logout() {
+    this.authStore.logout()
+    this.router.navigateByUrl('/')
+  }
 }
